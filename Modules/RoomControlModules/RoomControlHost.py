01a6d47e74d43071b0175f01c1d7e62e8464e0ec
JayFromProgramming <ajsweene@mtu.edu>
2024-02-26 18:02:53 -0500

    Adding menu bar

--- Modules/RoomControlModules/RoomControlHost.py
+++ Modules/RoomControlModules/RoomControlHost.py
@@ -19,9 +19,19 @@ class RoomControlHost(QLabel):
         self.dragging = False
         self.focused = False
         self.font = self.parent.get_font("JetBrainsMono-Bold")
+
+        # Scroll related variables
         self.scroll_offset = 0
         self.scroll_start = 0
-        self.last_scroll = 0
+        self.scroll_velocity = 0  # Pixels per second
+        self.scroll_velocity_decay = 0.8  # Percentage of velocity to keep each frame
+        self.scroll_max_velocity = 100  # Pixels per second
+        self.scroll_total_offset = 0
+        self.last_scroll = time.time()
+
+        self.scroll_motion_timer = QTimer(self)
+        self.scroll_motion_timer.timeout.connect(self.scroll_motion)
+
         self.has_schema = False
         self.setStyleSheet("boarder: 2px solid #ffcd00; border-radius: 10px")
         if os.path.exists("Config/auth.json"):
@@ -115,6 +125,7 @@ class RoomControlHost(QLabel):
             self.dragging = True
             self.scroll_start = event.pos().y()
             self.last_scroll = time.time()
+            self.scroll_motion_timer.stop()
         except Exception as e:
             logging.error(f"Error handling mouse press event: {e}")
             logging.exception(e)
@@ -124,9 +135,11 @@ class RoomControlHost(QLabel):
             if self.dragging:
                 # Offset the entire room control host by the difference in the y position
                 self.scroll_offset += ev.pos().y() - self.scroll_start
-                self.last_scroll = time.time()
-                self.move_widgets(self.scroll_offset)
+                # Calculate the velocity of the scroll
+                self.scroll_velocity = (ev.pos().y() - self.scroll_start) / (time.time() - self.last_scroll)
                 self.scroll_start = ev.pos().y()
+                # Move the widgets by the scroll offset
+                self.move_widgets(self.scroll_offset)
         except Exception as e:
             logging.error(f"Error handling mouse move event: {e}")
             logging.exception(e)
@@ -134,14 +147,39 @@ class RoomControlHost(QLabel):
     def mouseReleaseEvent(self, ev):
         try:
             self.dragging = False
-            # self.scroll_offset = 0
-            # self.move_widgets(self.scroll_offset)
+            # Start the scroll motion timer to decay the scroll velocity and move the widgets
             self.last_scroll = time.time()
+            self.scroll_motion_timer.start(round(1000 / 60))
         except Exception as e:
             logging.error(f"Error handling mouse release event: {e}")
             logging.exception(e)
 
+    def scroll_motion(self):
+        try:
+            # Decay the scroll velocity
+            self.scroll_velocity = max(-self.scroll_max_velocity, min(self.scroll_max_velocity, self.scroll_velocity))
+            self.scroll_velocity *= self.scroll_velocity_decay
+            # Move the widgets by the scroll velocity
+            self.scroll_offset += self.scroll_velocity
+            self.move_widgets(self.scroll_offset)
+            # If the scroll velocity is less than 1 pixel per second, stop the scroll motion timer
+            if abs(self.scroll_velocity) < 1:
+                self.scroll_motion_timer.stop()
+        except Exception as e:
+            logging.error(f"Error handling scroll motion: {e}")
+            logging.exception(e)
+
     def move_widgets(self, y):
+        y = round(y)
+        # Determine if this movement would cause the ungrouped device host to go off the top of the screen
+        if self.ungrouped_device_host.y() + y < 0:
+            y = -self.ungrouped_device_host.y()
+            self.scroll_velocity = 0
+        # Determine if this movement would cause the starred device host to go below its original position
+        if self.starred_device_host.y() + y > 0:
+            y = -self.starred_device_host.y()
+            self.scroll_velocity = 0
+        self.scroll_total_offset += y
         self.starred_device_host.move(20, y)
         y += self.starred_device_host.height() + 10
         for widget in self.device_group_hosts:
