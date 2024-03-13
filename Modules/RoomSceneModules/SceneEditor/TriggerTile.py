2aa8c3582235416c4ed7cec7dde458acfb5e47f2
JayFromProgramming <ajsweene@mtu.edu>
2024-03-13 14:05:41 -0400

    Adding Scene Support

--- Modules/RoomSceneModules/SceneEditor/TriggerTile.py
+++ Modules/RoomSceneModules/SceneEditor/TriggerTile.py
@@ -20,15 +20,18 @@ class TriggerTile(QLabel):
         self.trigger_name = QLabel(self)
         self.trigger_name.setFont(self.font)
         self.trigger_name.setFixedSize(170, 20)
-        self.trigger_name.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
-        self.trigger_name.setStyleSheet("color: black; font-size: 14px; font-weight: bold; border: none;")
-        self.trigger_name.setText(name)
+        self.trigger_name.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        self.trigger_name.setStyleSheet("color: black; font-size: 14px; font-weight: bold; border: none;"
+                                        "background-color: transparent")
+        self.trigger_name.setText(f"Name: {name}")
+        self.trigger_name.move(5, 0)
 
         self.trigger_data = QLabel(self)
         self.trigger_data.setFont(self.font)
         self.trigger_data.setFixedSize(170, 380)
         self.trigger_data.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
-        self.trigger_data.setStyleSheet("color: black; font-size: 12px; font-weight: bold; border: none; background-color: transparent")
+        self.trigger_data.setStyleSheet("color: black; font-size: 12px; font-weight: bold; border: none; "
+                                        "background-color: transparent")
         self.trigger_data.setText(f"Type: {data['trigger_type']}\nValue: {data['trigger_value']}")
         self.trigger_data.move(5, 20)
 
