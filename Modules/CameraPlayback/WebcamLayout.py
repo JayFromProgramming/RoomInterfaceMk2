ecc47de281edd6fc1073ccb863db56bf5259cea6
Jay <ajsweene@mtu.edu>
2024-06-27 09:16:26 -0400

    Spreading webcam loading over multiple frames to reduce lag when opening webcam panel

--- Modules/CameraPlayback/WebcamLayout.py
+++ Modules/CameraPlayback/WebcamLayout.py
@@ -15,6 +15,7 @@ except ImportError:
 class WebcamLayout(QLabel):
     min_cam_width = 400
     target_layout = (4, 3)
+    creation_delay = 50
 
     def __init__(self, parent):
         super().__init__(parent)
@@ -22,6 +23,7 @@ class WebcamLayout(QLabel):
         self.focused = False
 
         self.webcams = []
+        self.current_webcam = 0
         if not os.path.exists("Config/webcams.json"):
             with open("Config/webcams.json", "w") as f:
                 json.dump([], f)
@@ -36,27 +38,41 @@ class WebcamLayout(QLabel):
     def set_focus(self, focus) -> None:
         self.focused = focus
         if focus:
-            self.create_layout_timer.start(1)  # Use timer to preform layout creation in background
+            self.start_layout_creation()
             self.show()
         else:
             self.clear_layout()
             self.hide()
 
-    def create_layout(self):
+    def start_layout_creation(self):
         # self.clear_layout()
         self.webcams = []
         if WebcamWindow is None:
             return
-        for i, webcam in enumerate(self.webcam_file):
+        logging.info(f"Starting webcam layout creation for {len(self.webcam_file)} webcams")
+        self.current_webcam = 0
+        self.create_layout_timer.start(0)
+
+    def create_layout(self):
+        try:
+            webcam = self.webcam_file[self.current_webcam]
             if "thumb" not in webcam:
                 webcam["thumb"] = None
             webcam_window = WebcamWindow(self, webcam["url"], webcam["thumb"],
                                          webcam["title"], (self.width() / self.target_layout[0],
                                                            self.height() / self.target_layout[1]))
-            webcam_window.move((i % self.target_layout[0]) * webcam_window.width(),
-                               (i // self.target_layout[0]) * webcam_window.height())
+            webcam_window.move((self.current_webcam % self.target_layout[0]) * webcam_window.width(),
+                               (self.current_webcam // self.target_layout[0]) * webcam_window.height())
             self.webcams.append(webcam_window)
             webcam_window.show()
+            self.current_webcam += 1
+            if self.current_webcam < len(self.webcam_file):
+                self.create_layout_timer.start(self.creation_delay)
+            else:
+                self.update_layout()
+        except Exception as e:
+            logging.error(f"Failed to create webcam layout: {e}")
+            logging.exception(e)
 
     def update_layout(self):
         # Check if the webcam
